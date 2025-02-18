6d9ce627baefef8ca243ef6575695241df7bf9ef
giahuy215 <giahuy4842@gmail.com>
2025-02-18 18:21:54 +0700

    init project

---
+++ src/layouts/base-layout.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex lg:flex-row flex-col">
+		<div class="lg:w-2/3 w-full p-5">
+			<slot name="calendar"></slot>
+		</div>
+		<div class="lg:w-1/3 w-full p-5">
+			<slot name="sidebar"></slot>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
