2022b13773db9d6133c9176df5f860af75b029a2
giahuy215 <giahuy4842@gmail.com>
2025-02-19 19:56:02 +0700

    update UI appointment card

--- src/components/appointment-card.vue
+++ src/components/appointment-card.vue
@@ -3,13 +3,31 @@
 		<div class="w-2 bg-dark-orange rounded-l-lg"></div>
 		<div class="p-5 flex flex-row bg-dark-blue rounded-r-lg">
 			<div class="w-[70%] flex flex-col gap-2">
+				<!-- Title -->
 				<span class="text-lg text-white font-bold">
 					Vietnam 2025: Latest FDI Updates, Key Market Trends & Economic Outlook
 				</span>
+				<!-- Datetime -->
 				<span class="text-light-orange">Wed, 26 Feb, 16:30</span>
+				<div class="flex flex-row gap-3 items-center text-white">
+					<div class="w-10 h-10 p-2 rounded-full bg-dark-orange">
+						<img
+							class="w-full h-full"
+							src="../assets/user-white.svg"
+							alt="user-white"
+						/>
+					</div>
+					<span class="underline">View Client Profile</span>
+				</div>
 			</div>
 			<div class="w-[30%] flex justify-end">
-				<div class="w-10 h-10 rounded-full bg-dark-orange"></div>
+				<div class="w-14 h-14 p-2 rounded-full bg-dark-orange">
+					<img
+						class="w-full h-full"
+						src="../assets/camera-dark-blue.svg"
+						alt="camera-dark-blue"
+					/>
+				</div>
 			</div>
 		</div>
 	</div>
